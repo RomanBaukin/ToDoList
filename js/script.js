85642c2bd490c770c897d2b1d172aeed19a3c4ce
Roman Baukin <rbaukin1984@gmail.com>
2020-12-04 17:40:56 +0400

    Урок №12

---
+++ js/script.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const todoControl = document.querySelector('.todo-control'),
+  headerInput = document.querySelector('.header-input'),
+  todoList = document.querySelector('.todo-list'),
+  todoCompleted = document.querySelector('.todo-completed');
+
+const todoData = JSON.parse(localStorage.getItem('todoData'));
+
+const render = function () {
+  todoList.textContent = '';
+  todoCompleted.textContent = '';
+  localStorage.setItem('todoData', JSON.stringify(todoData));
+
+  todoData.forEach(function (item, index) {
+    const li = document.createElement('li');
+    li.classList.add('todo-item');
+
+    li.innerHTML =
+      '<span class="text-todo">' +
+      item.value +
+      '</span>' +
+      '<div class="todo-buttons">' +
+      '<button class="todo-remove"></button>' +
+      '<button class="todo-complete"></button>' +
+      '</div>';
+
+    if (item.completed) {
+      todoCompleted.append(li);
+    } else {
+      todoList.append(li);
+    }
+
+    const btnTodoComplete = li.querySelector('.todo-complete');
+    const btnTodoRemove = li.querySelector('.todo-remove');
+
+    btnTodoComplete.addEventListener('click', function () {
+      item.completed = !item.completed;
+      render();
+    });
+
+    btnTodoRemove.addEventListener('click', function () {
+      todoData.splice(index, 1);
+      render();
+    });
+  });
+};
+
+todoControl.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  if (headerInput.value !== '') {
+    const newTodo = {
+      value: headerInput.value,
+      completed: false,
+    };
+
+    todoData.push(newTodo);
+
+    render();
+
+    headerInput.value = '';
+  }
+});
+
+render();
